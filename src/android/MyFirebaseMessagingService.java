f6305b1f7bda3bd3c06d0302362da3debec24097
Muhamad Harith bin Rosli <harith5757@gmail.com>
2018-10-18 10:25:02 +0800

    Update to support oreo version
    update to support oreo version

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -1,15 +1,21 @@
 package com.gae.scaffolder.plugin;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.app.Notification;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -28,6 +34,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FCMPlugin";
 
+    private String getStringResource(String name) {
+        return this.getString(
+            this.getResources().getIdentifier(name, "string", this.getPackageName())
+        );
+    }
+
+    public class NotificationUtils extends ContextWrapper {
+        private NotificationManager mManager;
+        public static final String ANDROID_CHANNEL_ID = "announcement";
+        public static final String ANDROID_CHANNEL_NAME = "Announcement";
+        public NotificationUtils(Context base) {
+            super(base);
+            createChannels();
+        }
+
+        public void createChannels() {
+            // create android channel
+            NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+            ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            // Sets whether notifications posted to this channel should display notification lights
+            androidChannel.enableLights(true);
+            // Sets whether notification posted to this channel should vibrate.
+            androidChannel.enableVibration(true);
+            // Sets the notification light color for notifications posted to this channel
+            androidChannel.setLightColor(Color.GREEN);
+            // Sets whether notifications posted to this channel appear on the lockscreen or not
+            androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            getManager().createNotificationChannel(androidChannel);
+        }
+
+        private NotificationManager getManager() {
+            if (mManager == null) {
+                mManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            return mManager;
+        }
+    }
     /**
      * Called when message is received.
      *
@@ -68,6 +112,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String title, String messageBody, Map<String, Object> data, String imgUrl) {
+
         Log.d(TAG, "\tEnter Send notification method: ");
         Intent intent = new Intent(this, FCMPluginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -79,22 +124,57 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Bitmap bitmap = getBitmapfromUrl(imgUrl);
 		Log.d(TAG, "\tBitmap is running: ");
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(getApplicationInfo().icon)
-                .setContentTitle(title)
-                .setContentText(messageBody)
-                .setLargeIcon(bitmap)
-                .setStyle(new NotificationCompat.BigPictureStyle()
-                    .bigPicture(bitmap)
-                    .bigLargeIcon(null))/*Notification with Image*/
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("announcement", "Announcement",NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Announcement");
+            channel.enableLights(true);
+            // Sets whether notification posted to this channel should vibrate.
+            channel.enableVibration(true);
+            // Sets the notification light color for notifications posted to this channel
+            channel.setLightColor(Color.GREEN);
+            notificationManager.createNotificationChannel(channel);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(getApplicationInfo().icon)
+                    .setContentTitle(title)
+                    .setContentText(messageBody)
+                    .setLargeIcon(bitmap)
+                    .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(bitmap)
+                        .bigLargeIcon(null))/*Notification with Image*/
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setChannelId("announcement")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+            
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        }else{
+            
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(getApplicationInfo().icon)
+                    .setContentTitle(title)
+                    .setContentText(messageBody)
+                    .setLargeIcon(bitmap)
+                    .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(bitmap)
+                        .bigLargeIcon(null))/*Notification with Image*/
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+            
+            notificationManager.notify(100 /* ID of notification */, notificationBuilder.build());
+        }
+        //https://stackoverflow.com/questions/46990995/on-android-8-1-api-27-notification-does-not-display
+
     }
 
     /*
